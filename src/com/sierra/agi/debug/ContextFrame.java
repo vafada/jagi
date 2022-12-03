eac57f2d4df4142f8ee7af895a78208cca05684c
Mark <vafada@gmail.com>
2022-12-03 08:22:13 -1000

    use Swing. fix bottom and right edges getting cut off

--- src/com/sierra/agi/debug/ContextFrame.java
+++ src/com/sierra/agi/debug/ContextFrame.java
@@ -11,12 +11,17 @@ package com.sierra.agi.debug;
 import com.sierra.agi.awt.EgaComponent;
 import com.sierra.agi.logic.debug.LogicContextDebug;
 
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.KeyStroke;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 
-public class ContextFrame extends Frame implements ActionListener {
+public class ContextFrame extends JFrame implements ActionListener {
     protected LogicContextDebug logicContext;
     protected ContextDebugger debugger;
 
@@ -30,31 +35,34 @@ public class ContextFrame extends Frame implements ActionListener {
 
         this.logicContext = logicContext;
 
-        setMenuBar(addMenu());
+        setJMenuBar(addMenu());
         setFocusable(true);
         setFocusTraversalKeysEnabled(false);
         setResizable(false);
         pack();
 
         debugger = new ContextDebugger(logicContext);
+
+        logicContext.resumeExecution();
     }
 
-    protected MenuBar addMenu() {
-        MenuBar menubar = new MenuBar();
-        Menu menu;
-        MenuItem item;
+    protected JMenuBar addMenu() {
+        JMenuBar menubar = new JMenuBar();
+        JMenuItem item;
 
-        menu = new Menu("Engine");
-        item = new MenuItem("Start Execution");
+        JMenuItem menu = new JMenu("Engine");
+        item = new JMenuItem("Start Execution");
         item.setActionCommand("start");
         item.addActionListener(this);
-        item.setShortcut(new MenuShortcut(KeyEvent.VK_R, false));
+        item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_R, 0));
+        //item.setShortcut(new MenuShortcut(KeyEvent.VK_R, false));
         menu.add(item);
 
-        item = new MenuItem("Break Execution");
+        item = new JMenuItem("Break Execution");
         item.setActionCommand("break");
         item.addActionListener(this);
-        item.setShortcut(new MenuShortcut(KeyEvent.VK_R, true));
+        item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_R,java.awt.Event.SHIFT_MASK));
+        //item.setShortcut(new MenuShortcut(KeyEvent.VK_R, true));
         menu.add(item);
         menubar.add(menu);
 
